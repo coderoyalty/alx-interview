6d4fcca828eec161e878094d4b5e89425b648241
Akanni Emmanuel <akannie33@gmail.com>
2023-07-25 04:02:17 -0700

    pascal_triangle solution

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+pascal triangle function
+'''
+
+
+def pascal_triangle(num: int):
+    """
+    this function will compute the binomial coefficient of pascal triangle. 
+    """
+    if num <= 0:
+        return []
+    output = [[1]]
+
+    while len(output) != num:
+        arr = output[-1]
+        next_arr = [1]
+        for i in range(0, len(arr)):
+            if (i + 1) != len(arr):
+                val = arr[i] + arr[i + 1]
+            else:
+                val = arr[i]
+            next_arr.append(val)
+        output.append(next_arr)
+    return output
