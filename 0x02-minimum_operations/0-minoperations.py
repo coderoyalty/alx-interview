9e3abc838326228648fbaaab4614c0f66b43f6f0
Akanni Emmanuel <akannie33@gmail.com>
2023-08-17 01:52:35 -0700

    pep8 error correction

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,9 +7,10 @@
 def minOperations(n):
     """
     In a text file, there is a single character H.
-    Your text editor can execute only two operations in this file: Copy All and Paste.
-    Given a number n, write a method that calculates the fewest number of
-    operations needed to result in exactly n H characters in the file.
+    Your text editor can execute only two operations in this file:
+    Copy All and Paste. Given a number n, write a method that
+    calculates the fewest number of operations needed to result
+    in exactly n H characters in the file.
     """
 
     if type(n) != int:
